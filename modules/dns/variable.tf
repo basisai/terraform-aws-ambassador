ed4c9b8e3c22970ab0172325e54cd6094e5888fc
Yong Wen Chua <lawliet89@users.noreply.github.com>
2021-10-26 16:52:16 +0800

    Fix internal LB DNS Name

--- modules/dns/variable.tf
+++ modules/dns/variable.tf
@@ -6,6 +6,19 @@ variable "create_dns_records" {
 
 variable "lb_dns_name" {
   description = "LoadBalancer DNS name"
+  type        = string
+}
+
+variable "enable_l7_load_balancing" {
+  description = "Use L7 (ALB) for load balancing. Otherwise, L4 (NLB) is used"
+  type        = bool
+  default     = false
+}
+
+variable "internet_facing" {
+  description = "Whether the Load Balancer, L7 or L4 is internet facing"
+  type        = bool
+  default     = true
 }
 
 variable "dns_names" {
